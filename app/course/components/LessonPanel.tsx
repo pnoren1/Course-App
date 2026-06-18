b549c4ee04c279c5b83f5ac9b7454039e54d9972
Pnina Oren <pnoren1@gmail.com>
2026-06-18 23:42:55 +0300

    mark audio help

--- app/course/components/LessonPanel.tsx
+++ app/course/components/LessonPanel.tsx
@@ -132,7 +132,7 @@ export default function LessonPanel({ lesson, isOpen, userId, onVideoWatched }: 
             </div>
 
             {/* Audio Help Link */}
-            <AudioHelpLink />
+            {/* <AudioHelpLink /> */}
 
             {/* Internet Requirements Note */}
             <div className="bg-amber-50 border border-amber-200 rounded-lg p-4 mt-4">
